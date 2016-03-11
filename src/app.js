6d5b627c5d6486fbfb24196278a23a37e67e4a12
Christhio <christhioo@gmail.com>
2016-03-11 21:53:05 +0800

    1. Added total cost, discount, total cost after discount 2. Completed deployment to Heroku 3. Connected to ClearDB Database

--- src/app.js
+++ src/app.js
@@ -11,8 +11,7 @@ app.use(bodyParser.json()); // to support JSON-encoded bodies
 //import database.js
 var db = require('./database.js');
 
-//set host and port
-var host = '127.0.0.1';
+//set port
 var port = process.env.PORT || 3030;
 
 //create a new router object
@@ -22,6 +21,12 @@ var router = express.Router();
 router.use(function(req, res, next) {
     console.log('Processing your request...\n');
     next(); // make sure it doesn't stop here and go to the next routes
+});
+
+//display homepage
+app.get('/', function(req, res) {
+	res.send('Welcome! Please kindly refer to <a href="https://github.com/christhioo/rest-api-crud/blob/master/README.md">github</a>' +
+			 ' for instructions on how to access this API');
 });
 
 //list all items
@@ -38,16 +43,16 @@ router.get('/:username/items', function(req, res) {
 //add a new item
 router.post('/:username/items', function(req, res) {
 	if (!req.body.item_name || !req.body.price || !req.body.quantity) {
-		res.send({'status': 'error', 'message': 'missing a parameter'});
+		res.json({'status': 'error', 'message': 'missing a parameter'});
 	} else {
-		db.add_item(req, function(item, err){
+		db.add_item(req, function(items, err){
 			if (err) {
 				res.json({'message': err.code});
-			} else if (item.affectedRows === 1){
+			} else if (items.affectedRows === 1){
 				res.json({'message': 'Item has been successfully added!'});
 				console.log('Item has been successfully added!');
 			} else {
-				res.json(item.message);
+				res.json(items.message);
 				console.log('Item has failed to be added');
 			}
 		});
@@ -56,35 +61,64 @@ router.post('/:username/items', function(req, res) {
 
 //update the quantity of an existing item by item_id
 router.put('/:username/items/:item_id', function(req, res) {
-	db.update_item(req, function(item, err) {
+	db.update_item(req, function(items, err) {
 		if (err) {
 			res.json({'message': err.code});
-		} else if (item.affectedRows === 1){
+		} else if (items.affectedRows === 1){
 			res.json({'message': 'Item\'s quantity has been successfully updated'});
 			console.log('Item\'s quantity has been successfully updated');
 		} else {
-			res.json(item.message);
-			console.log(item.message);
+			res.json(items.message);
+			console.log(items.message);
 		}
 	});
 });
 
 //remove an existing item by item_id
 router.delete('/:username/items/:item_id', function(req, res) {
-	db.delete_item(req, function(item, err) {
+	db.delete_item(req, function(items, err) {
 		if (err) {
 			res.json({'message': err.code});
-		} else if (item.affectedRows === 1){
+		} else if (items.affectedRows === 1){
 			res.json({'message': 'Item has been successfully removed from database'});
 			console.log('Item has been successfully removed from database');
-		} else if (item.affectedRows === 0) {
+		} else if (items.affectedRows === 0) {
 			res.json({'message': 'Item is not found'});
 			console.log('Item is not found');
 		} else {
-			res.json(item.message);
+			res.json(items.message);
 			console.log('Item\'s removal has failed!');
 		}
 	});
+});
+
+//calculate the total items' price by username
+router.get('/:username/checkout', function(req, res) {
+	db.get_total_cost(req, function(items, err) {
+		if (err) {
+			res.json({'message': err.code});
+		} else {
+			res.json(items);
+		}
+	});
+});
+
+//get the total cost after being discounted by coupon code
+router.get('/:username/checkout/:coupon', function(req, res) {
+	
+	//get discount rate + total cost
+	db.get_discount(req, function(items, err) {
+		if (err) {
+			res.json({'message': err.code});
+		} else {
+			var after_discount = parseFloat(items["Total Cost"] * (1 - (items["Discount"]/100))).toFixed(2);
+			//add after_discount
+			items["Total Cost After Discount"] = after_discount;
+			
+			res.json(items);
+		}
+	});
+	
 });
 
 //all of routes will be prefixed with /api
@@ -92,4 +126,4 @@ app.use('/api', router);
 
 //start the server
 app.listen(port);
-console.log('Application is listening at host %s and port %s', host, port);
+console.log('Application is listening at port %s', port);
--- src/database.js
+++ src/database.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 var pool = mysql.createPool({
 	connectionLimit: 100,
-	host: 'localhost',
-	user: 'chris',
-	password: 'chris',
-	database: 'products'
+	host: 'us-cdbr-iron-east-03.cleardb.net',
+	user: 'bb1cf00a9d619a',
+	password: 'c2274ad1',
+	database: 'heroku_75f6a76935ccb16'
 });
 
 //================ Initialization for the database's tables ================//
@@ -14,11 +14,11 @@ var pool = mysql.createPool({
 var sql = 'CREATE TABLE IF NOT EXISTS items (' +
 			'item_id int NOT NULL AUTO_INCREMENT,' +
 			'item_name VARCHAR(255) NOT NULL,' +
-			'price DECIMAL NOT NULL,' +
+			'price DECIMAL(10,2) NOT NULL,' +
 			'quantity INT,' +
 			'user_account VARCHAR(64) NOT NULL,' +
 			'PRIMARY KEY (item_id)' +
-		  ')';
+		  ') AUTO_INCREMENT = 1001';
 
 //execute the sql query
 pool.query(sql, function(err, rows, fields) {
@@ -26,8 +26,8 @@ pool.query(sql, function(err, rows, fields) {
 	console.log('Successfully Created \'items\' Table');
 });
 
-//create another table to store discount coupon
-var sql = 'CREATE TABLE IF NOT EXISTS discount_coupon (' +
+//create another table to store coupon codes
+var sql = 'CREATE TABLE IF NOT EXISTS coupon_codes (' +
 			'coupon_code VARCHAR(32) NOT NULL,' +
 			'discount DECIMAL NOT NULL,' +
 			'PRIMARY KEY (coupon_code)' +
@@ -97,7 +97,7 @@ var update_item = function(req, callback) {
 	});
 };
 
-//delete item by item_id and user_account
+//delete an item by item_id and user_account
 var delete_item = function(req, callback) {
 	pool.getConnection(function(err, connection) {
 		var sql = 'DELETE FROM items WHERE item_id = ? AND user_account = ?';
@@ -115,8 +115,55 @@ var delete_item = function(req, callback) {
 	});
 };
 
+//get the total price of items of a user account
+var get_total_cost = function(req, callback) {
+	pool.getConnection(function(err, connection) {
+		var sql = 'SELECT SUM (price * quantity) AS "Total Cost" FROM items WHERE user_account = ?';
+		connection.query(sql, [req.params.username], function(err, rows) {
+			// done with the connection. 
+			connection.release();
+			
+			if (err){
+				return callback(500, err);
+			}
+			callback(rows);
+		});
+	});
+};
+
+var get_discount = function(req, callback) {
+	
+	pool.getConnection(function(err, connection) {
+		//get discount rate
+		var sql = 'SELECT discount FROM coupon_codes WHERE coupon_code = ?';
+		connection.query(sql, [req.params.coupon], function(err, rows) {
+			if (err){
+				return callback(500, err);
+			}
+			var discount = rows[0].discount;
+			
+			//get total cost
+			sql = 'SELECT SUM (price * quantity) AS "Total Cost" FROM items WHERE user_account = ?';
+			connection.query(sql, [req.params.username], function(err, rows) {
+				// done with the connection. 
+				connection.release();
+				
+				if (err){
+					return callback(500, err);
+				}
+				var total_cost = rows[0]["Total Cost"];
+				
+				//return discount and total cost
+				callback({"Discount": discount, "Total Cost": total_cost});
+			});
+		});
+	});
+};
+
 //export functions, so that it can be accessed by other files
 module.exports.get_all_items = get_all_items;
 module.exports.add_item = add_item;
 module.exports.update_item = update_item;
 module.exports.delete_item = delete_item;
+module.exports.get_total_cost = get_total_cost;
+module.exports.get_discount = get_discount;
